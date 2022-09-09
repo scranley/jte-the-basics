5fecd0e1b75e956be9c36e5d4cff5247f84a4946
scranley <80985721+scranley@users.noreply.github.com>
2022-09-09 15:56:44 -0700

    first commit

---
+++ pipeline_config.groovy
@@ -0,0 +1,10 @@
+/*
+  specify which libraries to load: 
+    In the Governance Tier configuration file, 
+    these should be configurations common across 
+    all apps governed by this config. 
+*/
+libraries{
+  merge = true 
+  sonarqube
+}
